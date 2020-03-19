f034210961d428e1d3b598472467e7e844b8ca40
Chad Rutherford <chad_rutherford@outlook.com>
2020-03-18 21:00:00 -0400

    Initial Commit

---
+++ CityAirportSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CityAirportSearch
+//
+//  Created by Chad Rutherford on 3/18/20.
+//  Copyright © 2020 Chad Rutherford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
